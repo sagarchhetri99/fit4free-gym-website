2307b8c0b5c14ed65065e003d948200c793b4371
sagarchhetri99 <73656587+sagarchhetri99@users.noreply.github.com>
2022-09-03 16:36:03 +0100

    implemented functionality on the horizontal bar
    users can now select any workout groups located in the horizontal bar which will then show the corresponsing workouts

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -8,6 +8,28 @@ export default function Exercises({ exercises, setExercises, bodyPart }) {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisePerPage = 9;
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   const indexOfLastExercise = currentPage * exercisePerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
   const currentExercise = exercises.slice(
@@ -23,7 +45,7 @@ export default function Exercises({ exercises, setExercises, bodyPart }) {
 
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
-      <Typography variant="h3" mb="46px">
+      <Typography variant="h3" mb="46px" align="center">
         Showing Results
       </Typography>
       <Stack
