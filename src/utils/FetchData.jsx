b2a13f12b0d4fc3fc6a17da9518905d83ba9adbe
sagarchhetri99 <73656587+sagarchhetri99@users.noreply.github.com>
2022-09-05 18:23:40 +0100

    getting a new api key

--- src/utils/FetchData.jsx
+++ src/utils/FetchData.jsx
@@ -1,8 +1,8 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
+    "X-RapidAPI-Key": "27efbfe8c6msh9b9520c209ec0f1p183e06jsna7a306e658d8",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
